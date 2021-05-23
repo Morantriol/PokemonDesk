e1119ed2bf6bc22ae9daac29afa35885253e4fa0
Yegor Anisimov <eanisimov96@gmail.com>
2021-05-24 00:26:31 +0500

    Победил что смог)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useRoutes } from 'hookrouter';
-import NotFoundPage from './components/404';
+import NotFoundPage from './pages/404';
 import routes from './routes';
+import Header from './components/Header';
 
 // import s from './App.module.scss';
 
 const App = () => {
   const match = useRoutes(routes);
-  return match || <NotFoundPage />;
+
+  return match ? (
+      <>
+        <Header />
+        { match }
+      </>
+  ) : <NotFoundPage />;
 };
 
 export default App;
